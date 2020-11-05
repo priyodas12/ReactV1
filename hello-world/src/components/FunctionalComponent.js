2237e13ebca3357ebd313fef14120614f3976f00
Priyobrato Das <priyodas12@gmail.com>
2020-11-05 08:19:47 +0530

    adding all files

---
+++ hello-world/src/components/FunctionalComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FunctionalComponent = () => {
+  return <h1>Hello, I am functional component!</h1>
+}
+// to disable named export uncomment below
+// export default FunctionalComponent
